a3aeeb9ebb642238158e3cd56d95d365b8beb992
Thu Vo <tv2842@truman.edu>
2018-09-23 19:33:52 -0500

    Add files via upload

---
+++ minPathSum.java
@@ -0,0 +1,22 @@
+package dynamic.programming;
+
+
+public class minPathSum {
+
+	public static void main(String[] args) {
+		int [][] grid = {{1,3,1,4},{1,5,1,5},{4,2,1,6}};
+		System.out.println("grid length: "+ grid.length);
+		System.out.println(solution(grid));
+	}
+	
+	public static int solution(int[][] grid) {
+		// Row length
+		int m = grid.length;
+		// Collum length
+		int n = grid[0].length;
+		
+		
+		return n;
+	}
+
+}
